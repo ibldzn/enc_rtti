51635957281a0b1f1c970f6fc2a5649b8914c99a
ibldzn <haytsam.adzka@gmail.com>
2021-11-27 16:59:16 +0700

    added screenshot

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 [![](https://img.shields.io/badge/github-ibldzn/enc__rtti-8da0cb?logo=github&style=for-the-badge)](https://github.com/ibldzn/enc_rtti)
 [![License: MIT](https://img.shields.io/badge/License-MIT-green.svg?style=for-the-badge)](https://opensource.org/licenses/MIT)
+
+## Before
+
+![Before1](res/before1.png)
+![Before2](res/before2.png)
+
+## After
+
+![After1](res/after1.png)
+![After2](res/after2.png)
--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,9 @@ fn main() -> Result<()> {
     ensure!(args.len() == 2, "Usage: {} <file>", args[0]);
 
     let mut buf = fs::read(&args[1]).with_context(|| format!("Failed to read {}", &args[1]))?;
+    let mut rng = rand::thread_rng();
 
-    let re = Regex::new(r"\.\?A[V|U](?P<data>.+?)@@\x00")?;
-    let type_desc_pos = re
+    let type_desc_pos = Regex::new(r"\.\?A[V|U](?P<data>.+?)@@\x00")?
         .captures_iter(&buf)
         .map(|m| m.name("data").unwrap().range())
         .collect::<Vec<Range<usize>>>();
@@ -24,11 +24,10 @@ fn main() -> Result<()> {
         .chain(b'a'..=b'z')
         .chain(b'0'..=b'9')
         .collect::<Vec<u8>>();
-    let mut rng = rand::thread_rng();
 
     for pos in type_desc_pos {
         for byte in &mut buf[pos] {
-            if alpha_num.contains(byte) {
+            if byte != &b'@' {
                 *byte = alpha_num[rng.gen_range(0..alpha_num.len())];
             }
         }
